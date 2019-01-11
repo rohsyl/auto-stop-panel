ccccaf798f559c1c558ff5aa14b835deca5c2627
Sylvain Roh <syzin12@gmail.com>
2019-01-11 09:06:13 +0100

    Comment code

--- app/Http/Controllers/AlertsController.php
+++ app/Http/Controllers/AlertsController.php
@@ -4,27 +4,36 @@ namespace App\Http\Controllers;
 
 use App\Firebase\FirebaseUtils;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
+use Kreait\Firebase;
 
+/**
+ * Class AlertsController. Manage Alerts
+ * @package App\Http\Controllers
+ */
 class AlertsController extends AuthController
 {
+    /**
+     * AlertsController constructor.
+     */
     public function __construct() {
         parent::__construct();
     }
 
+    /**
+     * This action will display the list of alerts
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index(){
 
+        // query to fb
         $firebase = FirebaseUtils::get();
-
         $database = $firebase->getDatabase();
-
         $references = $database->getReference('alerts');
-
         $alerts = $references->getValue();
 
         $readedAlerts = [];
         $newAlerts = [];
-
+        // split alert red by admin and the unred ones
         foreach ($alerts as $id => $alert){
             if($alert['readByAdmin']){
                 $readedAlerts[$id] = $alert;
@@ -40,19 +49,21 @@ class AlertsController extends AuthController
         ]);
     }
 
+    /**
+     * This action will display the details of a given alert
+     * @param $id int The id of the alert
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function details($id){
 
+        // query to fb
         $firebase = FirebaseUtils::get();
-
         $database = $firebase->getDatabase();
-
         $references = $database->getReference('alerts/'.$id);
-
         $alert = $references->getValue();
 
+        // load relationship data
         $this->fillAlertDetails($database, $alert);
-
-
 
         return view('alerts.details')->with([
             'alert' => $alert,
@@ -60,31 +71,42 @@ class AlertsController extends AuthController
         ]);
     }
 
+    /**
+     * Set the given as read
+     * @param $id int The if of the alert
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function read($id){
 
+        // query to fb
         $firebase = FirebaseUtils::get();
-
         $database = $firebase->getDatabase();
-
         $references = $database->getReference('alerts/'.$id);
-
         $alert = $references->getValue();
 
+        // set as read
         $alert['readDate'] = Carbon::now()->timestamp;
         $alert['readByAdmin'] = true;
 
+        // save change
         $references->set($alert);
 
         return redirect()->route('alerts.index');
     }
 
+    /**
+     * Fill the details of the alert
+     * @param $database Firebase\Database The reference to the fb database
+     * @param $alert array The alert to fill in
+     */
     private function fillAlertDetails($database, &$alert){
-        $references = $database->getReference('trips/'.$alert['tripUid']);
 
+        // load trip
+        $references = $database->getReference('trips/'.$alert['tripUid']);
         $alert['trip'] = $references->getValue();
 
+        // load owner
         $references = $database->getReference('persons/'.$alert['trip']['ownerUid']);
-
         $alert['trip']['owner'] = $references->getValue();
 
     }
--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,11 +1,16 @@
 <?php
-
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
+/**
+ * Class AuthController
+ * @package App\Http\Controllers
+ */
 class AuthController extends Controller
 {
+    /**
+     * AuthController constructor.
+     * This controller enable tu auth middleware so each controller that extend from this one will be secured
+     */
     public function __construct() {
         $this->middleware('auth');
     }
